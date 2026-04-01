9fc9ac5280b93cbe788ee0500ff1e03f295cbb5b
D.Shanmukasainath reddy <119025902+shanmukasainathreddy@users.noreply.github.com>
2026-04-01 22:38:49 +0530

    Delete springbootbasic directory

--- springbootbasic/src/main/java/com/capgemini/springbootbasic/CustomerController.java
+++
@@ -1,8 +0,0 @@
-package com.capgemini.springbootbasic;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class CustomerController {
-	 
-}
--- springbootbasic/src/main/java/com/capgemini/springbootbasic/DataNotExist.java
+++
@@ -1,5 +0,0 @@
-package com.capgemini.springbootbasic;
-
-public class DataNotExist extends RuntimeException {
-	
-}
